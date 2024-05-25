b759b9c7211ad43916eaf7daec9b447e48bc2adc
Yehya muhammed <muhammedyehya60@example.com>
2024-05-25 09:29:52 +0300

    need some change

--- src/Color.jsx
+++ src/Color.jsx
@@ -1,14 +1,18 @@
 import {useState} from "react";
 
 function Color() {
-    const [color, setColor] = useState("red");
+    const [color, setColor] = useState("");
     const handleColorChange = (e) => {
         setColor(e.target.value);
     }
-return <>
-    <div className="bg-info w-25">{color}</div>
-    <input type="color" onChange = {handleColorChange}/>
-</>
+return <div className="flex-column align-items-center">
+    <h1 className="m-2">select a color</h1>
+    <div style={{backgroundColor: color}} className="w-25 h-100 flex-row justify-content-center align-items-center">
+        <p>selected color: {color}</p>
+    </div>
+    <input type="color" onChange={handleColorChange} value={color}/>
+
+</div>
 }
 
 export default Color;
